62e3387a64aaefbb756be6f24d0f5ac403974d7f
numilani <numilani@protonmail.com>
2025-06-10 23:50:04 -0400

    wip

--- src/main/java/me/numilani/rpchef/BaseRecipe.java
+++ src/main/java/me/numilani/rpchef/BaseRecipe.java
@@ -1,5 +1,18 @@
 package me.numilani.rpchef;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.Material;
+
 public class BaseRecipe {
+  public String Name = "";
+  public Material ResultType;
+  public List<String> DescriptiveStarters = new ArrayList<String>();
+
+  public List<Ingredient> BaseIngredients = new ArrayList<Ingredient>();
+  public HashMap<Ingredient, Integer> AdditionalIngredients = new HashMap<Ingredient, Integer>(); 
+
 
 }
--- src/main/java/me/numilani/rpchef/Ingredient.java
+++ src/main/java/me/numilani/rpchef/Ingredient.java
@@ -6,26 +6,27 @@ import java.util.List;
 import org.bukkit.Material;
 
 public class Ingredient {
-  public String RecipeName = "";
+  public String Name = "";
   public Material ItemType;
+  public String FoodTypeTag;
   public List<String> LoreLines = new ArrayList<String>();
 
   public List<String> Adjectives = new ArrayList<String>();
   public List<String> DescriptiveSentences = new ArrayList<String>();
 
   public Ingredient(String name, String type){
-    RecipeName = name;
+    Name = name;
     ItemType = Material.getMaterial(type);
   }
 
   public Ingredient(String name, String type, List<String> lore){
-    RecipeName = name;
+    Name = name;
     ItemType = Material.getMaterial(type);
     LoreLines = lore;
   }
  
   public Ingredient(String name, String type, List<String> lore, List<String> adjs, List<String> descripts){
-    RecipeName = name;
+    Name = name;
     ItemType = Material.getMaterial(type);
     LoreLines = lore;
     Adjectives = adjs;
